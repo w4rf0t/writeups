d5a28e13650c395a942316dd64cdd549153252f6
w4rf0t <hkphu.edu@gmail.com>
2022-10-30 12:30:39 +0700

    src="https://giscus.app/client.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -41,10 +41,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'w4rf0t/writeups',
+      repositoryId: 'R_kgDOIVg1Zg',
+      category: 'General',
+      categoryId: 'DIC_kwDOIVg1Zs4CSRgO',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
