d90004b3b34c3afb9225921932237e591d79a8bc
w4rf0t <hkphu.edu@gmail.com>
2022-10-30 13:40:20 +0700

    vl

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,16 +3,16 @@ const projectsData = [
     title: 'SRC CTF playground',
     description: `I have hold up this CTF playground. It was built base on CTFd.io, modified by w4rf0t anh then deployed on Azure. I also uploaded it into Github for everyone to use.`,
     imgSrc: 'https://user-images.githubusercontent.com/61643034/198817144-e2ae6933-470b-4f56-ad03-0613431b4676.png',
-    href: 'https://github.com/w4rf0t/SRC_CTF_servercode#readme',
+    href: '/static/images/ctf_src.png',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
