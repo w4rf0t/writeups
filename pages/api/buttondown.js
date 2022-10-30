e86cce080f8448923c4fce78343bd17982babc7a
w4rf0t <hkphu.edu@gmail.com>
2022-10-30 15:06:28 +0700

    vk

--- pages/api/buttondown.js
+++ pages/api/buttondown.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/no-anonymous-default-export
+eslint-disable-next-line import/no-anonymous-default-export
+
 export default async (req, res) => {
   const { email } = req.body
   if (!email) {
