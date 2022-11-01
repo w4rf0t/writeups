1b8131a998042aeae142c15bd4c0ed40519f89c2
w4rf0t <hkphu.edu@gmail.com>
2022-11-01 14:14:36 +0700

    {/* <ul>           {!filteredBlogPosts.length && 'No posts found.'}           {displayPosts.map((frontMatter) => {             const { slug, date, title, summary, tags } = frontMatter             return (               <li key={slug} className="py-4">                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">                   <dl>                     <dt className="sr-only">Published on</dt>                     <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">                       <time dateTime={date}>{formatDate(date)}</time>                     </dd>                   </dl>                   <div className="space-y-3 xl:col-span-3">                     <div>                       <h3 className="text-2xl font-bold leading-8 tracking-tight">                         <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">                           {title}                         </Link>                       </h3>                       <div className="flex flex-wrap">                         {tags.map((tag) => (                           <Tag key={tag} text={tag} />                         ))}                       </div>                     </div>                     <div className="prose max-w-none text-gray-500 dark:text-gray-400">                       {summary}                     </div>                   </div>                 </article>               </li>             )           })}         </ul> */}

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -49,41 +49,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
             </svg>
           </div>
         </div>
-        {/* <ul>
-          {!filteredBlogPosts.length && 'No posts found.'}
-          {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-3 xl:col-span-3">
-                    <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
-                        </Link>
-                      </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
-                    </div>
-                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                      {summary}
-                    </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul> */}
+        
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => (
