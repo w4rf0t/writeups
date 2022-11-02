9fc2c95c29014fbc2fa863569824975d8863c6a1
w4rf0t <hkphu.edu@gmail.com>
2022-11-02 17:06:47 +0700

    hehe

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export async function getStaticProps() {
   return { props: { posts } }
 }
 
-export default function HEHE({ posts }) {
+export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -118,17 +118,6 @@ export default function HEHE({ posts }) {
           <NewsletterForm />
         </div>
       )}
-    </>
-  )
-}
-
-export default function Home({ pinnedItems }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -150,20 +139,7 @@ export default function Home({ pinnedItems }) {
           })}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
 
